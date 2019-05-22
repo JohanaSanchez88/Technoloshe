8a43acbf8e592dfd36b2fd1affbc5e294a6392c6
technology <technology@CPX-U6CLM3L734A.dir.svc.accenture.com>
2019-05-21 20:29:15 -0500

    inicio del proyecto

---
+++ TechonolosShe/TechonolosShe/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechonolosShe
+{
+    public class Class1
+    {
+        long valor = 0;
+        public long metodo()
+        {
+            for (long i=0; i<100000000; i ++)
+            {
+                valor += i;
+            }
+            return valor;
+        }
+    }
+}
---
+++ TechonolosShe/TechonolosShe/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TechonolosShe
+{
+    public partial class Form1 : Form
+    {
+        private int contador;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Class1 clase = new Class1();
+            
+            long c = clase.metodo();
+
+            label1.Text = c.ToString();
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            contador += 1;
+            label2.Text = contador.ToString();
+        }
+    }
+}
